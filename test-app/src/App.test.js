9d43ee5491348b11a88a73f740d067627d626ad5
= <=>
2022-04-08 08:59:58 +0200

    testing-library basic

---
+++ test-app/src/App.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// в официальной документации есть перечень тэгов с role по умолчанию
+test('renders 3 list items', () => {
+  render(<App />);
+  const listItems = screen.getAllByRole('listitem');
+  expect(listItems).toHaveLength(3);
+});
+
+test('renders title', () => {
+  render(<App />);
+  const title = screen.getByTestId('mytestid');
+  expect(title).toBeInTheDocument();
+});
+
+test('sum should be 5', () => {
+  render(<App />);
+  const sum = screen.getByTitle('sum');
+  expect(sum.textContent).toBe('5');
+});
